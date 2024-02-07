323f2c2223120a86ca212679c29c49f3d6eb73ae
arorachirag <96038346+arorachirag@users.noreply.github.com>
2024-02-07 21:05:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function fibonacci(num) {
 // your code here
+	if(c==0) return 0;
+	let prev = 0;
+	let curr = 1;
+
+	for (let i = 2; i <= num; i++) {
+		let next = prev + curr;
+		prev = curr;
+		curr = next;
+	}
+
+	return prev;
 }
 
 module.exports = fibonacci;
